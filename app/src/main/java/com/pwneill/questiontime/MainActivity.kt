fdef9495ce0c50a65e5d9050be4452d0a91f09da
Pwneill <phelan.oneill@gmail.com>
2021-01-11 01:12:32 -0600

    added true/false lambda click listeners

--- app/src/main/java/com/pwneill/questiontime/MainActivity.kt
+++ app/src/main/java/com/pwneill/questiontime/MainActivity.kt
@@ -2,10 +2,51 @@ package com.pwneill.questiontime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fun onClick(v: View?) {
+
+            when (v?.id) {
+                R.id.trueBtn -> {
+                    Log.i("btn", "tueBtn was tapped")
+                }
+                R.id.falseBtn -> {
+                    Log.i("btn", "falseBtn was tapped")
+                }
+            }
+        }
+
+
+        val trueBtn: Button = findViewById(R.id.trueBtn);
+        trueBtn.setOnClickListener{ v: View? -> onClick(v) }
+
+        val falseBtn: Button = findViewById(R.id.falseBtn);
+        falseBtn.setOnClickListener{ v: View? -> onClick(v) }
+
+//            trueBtn.set
+
+//            trueBtn.setOnClickListener(onClick(View v))
+//
+//        trueBtn.setOnClickListener() {
+//
+//            Log.i("btn", "true was clicked")
+//
+//        }
+//
+//        val falseBtn: Button = findViewById(R.id.falseBtn);
+//            falseBtn.setOnClickListener()
+//
+//        falseBtn.setOnClickListener() {
+//
+//            Log.i("btn", "false was clicked")
+
+        }
+
     }
-}
