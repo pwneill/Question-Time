27376b63cb9ba93a01c1b1e62f9328b45febcb17
Pwneill <phelan.oneill@gmail.com>
2021-01-11 22:59:40 -0600

    added Toast alerts, begun generating questionText on screen

--- app/src/main/java/com/pwneill/questiontime/MainActivity.kt
+++ app/src/main/java/com/pwneill/questiontime/MainActivity.kt
@@ -2,50 +2,53 @@ package com.pwneill.questiontime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val q1 = QuizModel(R.string.q1, true)
+    val q2 = QuizModel(R.string.q2, true)
+    val q3 = QuizModel(R.string.q3, true)
+    val q4 = QuizModel(R.string.q4, true)
+    val q5 = QuizModel(R.string.q5, true)
+    val q6 = QuizModel(R.string.q6, true)
+    val q7 = QuizModel(R.string.q7, true)
+    val q8 = QuizModel(R.string.q8, true)
+    val q9 = QuizModel(R.string.q9, true)
+    val q10 = QuizModel(R.string.q10, true)
+
+    private val questionBank: Array<QuizModel> = arrayOf(q1, q2, q3, q4, q5, q6, q7, q8, q9, q10)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.txtQuestion);
+        textView?.text = "${getString(questionBank.get(0).question)} ";
+
 
         fun onClick(v: View?) {
 
             when (v?.id) {
                 R.id.trueBtn -> {
-                    Log.i("btn", "tueBtn was tapped")
+                    Toast.makeText(this, "trueBtn was tapped", Toast.LENGTH_SHORT).show()
                 }
                 R.id.falseBtn -> {
-                    Log.i("btn", "falseBtn was tapped")
+                    Toast.makeText(this, "falseBtn was tapped", Toast.LENGTH_SHORT).show()
+
                 }
             }
         }
-
 
         val trueBtn: Button = findViewById(R.id.trueBtn);
         trueBtn.setOnClickListener{ v: View? -> onClick(v) }
 
         val falseBtn: Button = findViewById(R.id.falseBtn);
         falseBtn.setOnClickListener{ v: View? -> onClick(v) }
-
-//            trueBtn.set
-
-//            trueBtn.setOnClickListener(onClick(View v))
-//
-//        trueBtn.setOnClickListener() {
-//
-//            Log.i("btn", "true was clicked")
-//
-//        }
-//
-//        val falseBtn: Button = findViewById(R.id.falseBtn);
-//            falseBtn.setOnClickListener()
-//
-//        falseBtn.setOnClickListener() {
-//
-//            Log.i("btn", "false was clicked")
 
         }
 
---
+++ app/src/main/java/com/pwneill/questiontime/QuizModel.kt
@@ -0,0 +1,9 @@
+package com.pwneill.questiontime
+
+class QuizModel(question: Int, answer: Boolean) {
+
+   val question: Int = question
+   val answer: Boolean = answer
+
+
+}
